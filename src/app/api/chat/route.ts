880a33a96587fd9f66042ddcea98f0e13a053305
Yerassyl <serikyer0204@gmail.com>
2024-06-23 13:02:33 +0500

    fixed api

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -18,7 +18,7 @@ export async function POST(request: Request){
         model: 'gpt-3.5-turbo',
         stream: true,
         messages: [
-        {role:"system", content: 'You are an AI Counselor for admission company named Impact, which gives full maintenance for their students. You need to advise clients and help them with basic questions about admissions in top universities. Try to advertise the courses and free consulting appointments of the website.'},
+        {role:"system", content: 'You are an AI Counselor for admission company located in Kazakhstan named Impact, which gives full maintenance for their students. You need to advise clients and help them with basic questions about admissions in top universities. Try to advertise the courses and free consulting appointments of the website.'},
             ...messages
         ],
         max_tokens: 512,
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -4,14 +4,9 @@ import Footer from "@/components/Footer";
 export default function Explain() {
   const courseData = [
     {
-      title: "Course 1",
-      description: "Description for course 1",
+      title: "",
+      description: "",
       youtubeId: "qrmwZdWzfvs?si=X6LvSjK092aB12vz"
-    },
-    {
-      title: "Course 2",
-      description: "Description for course 2",
-      youtubeId: "06A5X87nA_M?si=mCAN_Aq-49El1oX2"
     },
   ];
 
